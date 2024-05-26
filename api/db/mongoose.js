ee35192997ec5317d9a4116552ec5fd78804d408
Iamshivambatra <mihirbatra65@gmail.com>
2024-05-26 12:48:05 +0530

    mongo atlas errors resolved

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,7 +2,13 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
 dotenv.config();
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  tlsAllowInvalidCertificates: false,
+});
+// mongoose.connect('mongodb://127.0.0.1:27017/airbnb_DB?directConnection=true');
+
 
 const db = mongoose.connection;
 
