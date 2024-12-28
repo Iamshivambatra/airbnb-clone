68faaf2c9071c59e1841ab9e24c58ce8c121de2f
mihir batra <93129686+Iamshivambatra@users.noreply.github.com>
2024-12-28 12:05:37 +0530

    Update App.js

--- airbnb_frontend/src/App.js
+++ airbnb_frontend/src/App.js
@@ -16,10 +16,7 @@ import Placepage from './pages/placePage';
 import Placesformpage from './pages/placesformpage';
 import Bookings from './pages/bookings';
 
-axios.defaults.baseURL =
-  process.env.NODE_ENV === "production"
-    ? "https://airbnb-clone-2-fqnc.onrender.com"
-    : "http://localhost:8000";
+axios.defaults.baseURL = "https://airbnb-clone-2-fqnc.onrender.com" ||"http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 
